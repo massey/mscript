d9ea34fadd8c413a567b67ae37bca8693f22c731
Matt Andrews <mattandrews@massey.com.au>
2017-07-09 15:52:40 +1000

    Added parse function to exports

--- src/mscript.ts
+++ src/mscript.ts
@@ -12,6 +12,10 @@ export function interpret
   return  i.compile()
 }
 
+export function parse (input: string): Node {
+  return acorn.parse(input) as Node
+}
+
 export function transpile (input: string, options: {savedData?: SavedData, parent?: any}): Node {
   let ast = acorn.parse(input)
   let i   = new Interpreter(ast as Node)
